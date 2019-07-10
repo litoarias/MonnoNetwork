3d08e90c3faa31c726078128a0682a956a963e5c
Lito <hipolito.arias.cervero.ext@ing.com>
2019-07-10 09:23:06 +0200

    update logs and issues

--- MonnoNetwork/NetworkingService.swift
+++ MonnoNetwork/NetworkingService.swift
@@ -14,10 +14,18 @@ import Foundation
 /// - badResponse: <#badResponse description#>
 /// - badEncoding: <#badEncoding description#>
 public enum NetworkingError: Error {
-    case badUrl
-    case badResponse
-    case badEncoding
-    case unknown(Data)
+	case badUrl
+	case badResponse
+	case badEncoding
+	case serializationError(SerializationError)
+	case serverError
+	case unknown(Data)
+}
+
+public enum SerializationError: Error {
+	case nilData
+	case emptyData
+	case cannotSerialize
 }
 
 /// <#Description#>
@@ -25,8 +33,8 @@ public enum NetworkingError: Error {
 /// - get: <#get description#>
 /// - post: <#post description#>
 public enum HTTPMethod {
-    case get
-    case post
+	case get
+	case post
 }
 
 
@@ -36,156 +44,159 @@ public typealias Parameters = [String: Any]
 
 /// <#Description#>
 public protocol Networking {
-    var session: URLSession? { get set }
-    var baseUrl: String { get set }
-    func handleResponse<T: Decodable>(for request: URLRequest, completion: @escaping (Result<(object: T, unwrapped: Data), Error>) -> Void)
-    func request<T: Decodable>(method: String, path: String, headers: Headers?, urlParams: Parameters?, completion: @escaping (Result<(object: T, unwrapped: Data), Error>) -> Void)
-    func request<T: Decodable>(method: String, path: String, headers: Headers?, bodyParams: Parameters?, completion: @escaping (Result<(object: T, unwrapped: Data), Error>) -> Void)
-    func call<T: Decodable>(path: String, headers: Headers?, params: Parameters?, httpMethod: HTTPMethod, completion: @escaping (Result<(object: T, unwrapped: Data), Error>) -> Void)
+	var session: URLSession? { get set }
+	var baseUrl: String { get set }
+	func handleResponse<T: Decodable>(for request: URLRequest, completion: @escaping (Result<(object: T?, unwrapped: Data), Error>) -> Void)
+	func request<T: Decodable>(method: String, path: String, headers: Headers?, urlParams: Parameters?, completion: @escaping (Result<(object: T?, unwrapped: Data), Error>) -> Void)
+	func request<T: Decodable>(method: String, path: String, headers: Headers?, bodyParams: Parameters?, completion: @escaping (Result<(object: T?, unwrapped: Data), Error>) -> Void)
+	func call<T: Decodable>(path: String, headers: Headers?, params: Parameters?, httpMethod: HTTPMethod, completion: @escaping (Result<(object: T?, unwrapped: Data), Error>) -> Void)
 }
 
 public extension Networking {
-    
-    
-    /// <#Description#>
-    ///
-    /// - Parameters:
-    ///   - request: <#request description#>
-    ///   - completion: <#completion description#>
-    func handleResponse<T: Decodable>(for request: URLRequest, completion: @escaping (Result<(object: T, unwrapped: Data), Error>) -> Void) {
-        
-        #if DEBUG
-        print("REQUEST:\n")
-        dump(request)
-        print("-----------------")
-        #endif
-        
-        let task = session?.dataTask(with: request) { (data, response, error) in
-            DispatchQueue.main.async {
-                #if DEBUG
-                print("DATA:\n")
-                dump(String(data: data ?? Data(), encoding: .utf8))
-                print("-----------------")
-                print("RESPONSE:\n")
-                dump(response)
-                print("-----------------")
-                print("ERROR: -----------------")
-                dump(error)
-                print("-----------------")
-                #endif
-                
-                //                guard let unwrappedResponse = response as? HTTPURLResponse else {
-                //                    completion(.failure(NetworkingError.badResponse))
-                //                    return
-                //                }
-                if let unwrappedError = error {
-                    completion(.failure(unwrappedError))
-                    return
-                }
-                if let unwrappedData = data {
-                    do {
-                        let object = try JSONDecoder().decode(T.self, from: unwrappedData)
-                        completion(.success((object, unwrappedData)))
-                    } catch {
-                        completion(.failure(NetworkingError.unknown(unwrappedData)))
-                    }
-                }
-            }
-        }
-        task?.resume()
-    }
-    
-    
-    /// <#Description#>
-    ///
-    /// - Parameters:
-    ///   - method: <#method description#>
-    ///   - path: <#endpoint description#>
-    ///   - headers: <#headers description#>
-    ///   - urlParams: <#urlParams description#>
-    ///   - completion: <#completion description#>
-    func request<T: Decodable>(method: String, path: String, headers: Headers?, urlParams: Parameters?, completion: @escaping (Result<(object: T, unwrapped: Data), Error>) -> Void) {
-        guard let url = URL(string: baseUrl) else {
-            completion(.failure(NetworkingError.badUrl))
-            return
-        }
-        var components = URLComponents()
-        components.scheme = url.scheme
-        components.host = url.host
-        components.path = path
-        
-        var queryItems = [URLQueryItem]()
-        
-        if let params = urlParams {
-            for (key, value) in params {
-                let queryItem = URLQueryItem(name: key, value: value as? String)
-                queryItems.append(queryItem)
-            }
-        }
-        
-        components.queryItems = queryItems
-        
-        guard let urlQuery = components.url else {
-            completion(.failure(NetworkingError.badUrl))
-            return
-        }
-        var request = URLRequest(url: urlQuery)
-        request.httpMethod = method
-        if let head = headers {
-            for (key, value) in head {
-                request.addValue(value, forHTTPHeaderField: key)
-            }
-        }
-        handleResponse(for: request, completion: completion)
-    }
-    
-    
-    /// <#Description#>
-    ///
-    /// - Parameters:
-    ///   - method: <#method description#>
-    ///   - endpoint: <#endpoint description#>
-    ///   - headers: <#headers description#>
-    ///   - bodyParams: <#bodyParams description#>
-    ///   - completion: <#completion description#>
-    func request<T: Decodable>(method: String, path: String, headers: Headers?, bodyParams: Parameters?, completion: @escaping (Result<(object: T, unwrapped: Data), Error>) -> Void) {
-        guard let url = URL(string: baseUrl + path) else {
-            completion(.failure(NetworkingError.badUrl))
-            return
-        }
-        var request = URLRequest(url: url)
-        if let body = bodyParams {
-            do {
-                let jsonData = try JSONSerialization.data(withJSONObject: body, options: [])
-                request.httpBody = jsonData
-            } catch {
-                completion(.failure(NetworkingError.badEncoding))
-            }
-        }
-        request.httpMethod = method
-        if let head = headers {
-            for (key, value) in head {
-                request.addValue(value, forHTTPHeaderField: key)
-            }
-        }
-        handleResponse(for: request, completion: completion)
-    }
-    
-    
-    /// <#Description#>
-    ///
-    /// - Parameters:
-    ///   - path: <#path description#>
-    ///   - headers: <#headers description#>
-    ///   - params: <#params description#>
-    ///   - httpMethod: <#httpMethod description#>
-    ///   - completion: <#completion description#>
-    func call<T: Decodable>(path: String, headers: Headers?, params: Parameters?, httpMethod: HTTPMethod, completion: @escaping (Result<(object: T, unwrapped: Data), Error>) -> Void) {
-        switch httpMethod {
-        case .post:
-            request(method: "POST", path: path, headers: headers, bodyParams: params, completion: completion)
-        case .get:
-            request(method: "GET", path: path, headers: headers, urlParams: params, completion: completion)
-        }
-    }
+	
+	
+	/// <#Description#>
+	///
+	/// - Parameters:
+	///   - request: <#request description#>
+	///   - completion: <#completion description#>
+	func handleResponse<T: Decodable>(for request: URLRequest, completion: @escaping (Result<(object: T?, unwrapped: Data), Error>) -> Void) {
+		
+		let task = session?.dataTask(with: request) { (data, taskResponse, taskError) in
+			DispatchQueue.main.async {
+				#if DEBUG
+				debugPrint("======================== BEGIN REQUEST ========================")
+				debugPrint(request.taskDescription())
+				debugPrint("========================= END REQUEST =========================")
+				#endif
+				
+				guard taskError == nil else {
+					completion(.failure(taskError!))
+					return
+				}
+				
+				guard let response = taskResponse as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+					completion(.failure(NetworkingError.serverError))
+					return
+				}
+				
+				debugPrint("======================== BEGIN RESPONSE ========================")
+				debugPrint(response.taskDescription(data))
+				debugPrint("========================= END RESPONSE =========================")
+				
+				//	guard let mime = response.mimeType, mime == "application/json" else {
+				//		print("Wrong MIME type!")
+				//		return
+				//	}
+				
+				guard let data = data else {
+					completion(.failure(NetworkingError.serializationError(.nilData)))
+					return
+				}
+				
+				do {
+					let object = try JSONDecoder().decode(T.self, from: data)
+					completion(.success((object, data)))
+				} catch {
+					completion(.success((nil, data)))
+				}
+				
+			}
+		}
+		task?.resume()
+	}
+	
+	
+	/// <#Description#>
+	///
+	/// - Parameters:
+	///   - method: <#method description#>
+	///   - path: <#endpoint description#>
+	///   - headers: <#headers description#>
+	///   - urlParams: <#urlParams description#>
+	///   - completion: <#completion description#>
+	func request<T: Decodable>(method: String, path: String, headers: Headers?, urlParams: Parameters?, completion: @escaping (Result<(object: T?, unwrapped: Data), Error>) -> Void) {
+		guard let url = URL(string: baseUrl) else {
+			completion(.failure(NetworkingError.badUrl))
+			return
+		}
+		
+		var components = URLComponents()
+		components.scheme = url.scheme
+		components.host = url.host
+		components.path = path
+		
+		var queryItems = [URLQueryItem]()
+		if let params = urlParams {
+			for (key, value) in params {
+				let queryItem = URLQueryItem(name: key, value: value as? String)
+				queryItems.append(queryItem)
+			}
+		}
+		components.queryItems = queryItems
+		
+		guard let urlQuery = components.url else {
+			completion(.failure(NetworkingError.badUrl))
+			return
+		}
+		
+		var request = URLRequest(url: urlQuery)
+		request.httpMethod = method
+		if let head = headers {
+			for (key, value) in head {
+				request.addValue(value, forHTTPHeaderField: key)
+			}
+		}
+		handleResponse(for: request, completion: completion)
+	}
+	
+	
+	/// <#Description#>
+	///
+	/// - Parameters:
+	///   - method: <#method description#>
+	///   - endpoint: <#endpoint description#>
+	///   - headers: <#headers description#>
+	///   - bodyParams: <#bodyParams description#>
+	///   - completion: <#completion description#>
+	func request<T: Decodable>(method: String, path: String, headers: Headers?, bodyParams: Parameters?, completion: @escaping (Result<(object: T?, unwrapped: Data), Error>) -> Void) {
+		guard let url = URL(string: baseUrl + path) else {
+			completion(.failure(NetworkingError.badUrl))
+			return
+		}
+		var request = URLRequest(url: url)
+		if let body = bodyParams {
+			do {
+				let jsonData = try JSONSerialization.data(withJSONObject: body, options: [])
+				request.httpBody = jsonData
+			} catch {
+				completion(.failure(NetworkingError.badEncoding))
+			}
+		}
+		request.httpMethod = method
+		if let head = headers {
+			for (key, value) in head {
+				request.addValue(value, forHTTPHeaderField: key)
+			}
+		}
+		handleResponse(for: request, completion: completion)
+	}
+	
+	
+	/// <#Description#>
+	///
+	/// - Parameters:
+	///   - path: <#path description#>
+	///   - headers: <#headers description#>
+	///   - params: <#params description#>
+	///   - httpMethod: <#httpMethod description#>
+	///   - completion: <#completion description#>
+	func call<T: Decodable>(path: String, headers: Headers?, params: Parameters?, httpMethod: HTTPMethod, completion: @escaping (Result<(object: T?, unwrapped: Data), Error>) -> Void) {
+		switch httpMethod {
+		case .post:
+			request(method: "POST", path: path, headers: headers, bodyParams: params, completion: completion)
+		case .get:
+			request(method: "GET", path: path, headers: headers, urlParams: params, completion: completion)
+		}
+	}
 }
--- NetworkTestApp/ViewController.swift
+++ NetworkTestApp/ViewController.swift
@@ -10,84 +10,58 @@ import UIKit
 import MonnoNetwork
 
 class ViewController: UIViewController {
-   
-    let mocks: Bool = false
-    var networkingService: Networking {
-//        return self.mocks ? NetworkingMockService() : NetworkingService(baseUrl: "https://jsonplaceholder.typicode.com/posts")
-        return self.mocks ? NetworkingMockService() : NetworkingService(baseUrl: "http://argo.cdragota.lab.cloudioo.net")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    func dataRequest() {
-
-        let preferredLanguage = NSLocale.preferredLanguages[0]
-        
-        
-//        networkingService.call(path: "/api_contents/get/wallpapers_categories", headers: nil, params: ["lang":preferredLanguage], httpMethod: .get) { (result: Result<[Tag], Error>) in
-//            switch result {
-//            case .success(let post):
-//                print(post)
-//                let alert = UIAlertController(title: "SUCCESS", message: "\(String(describing: post))", preferredStyle: .alert)
-//
-//                let act = UIAlertAction(title: "Yes", style: .default, handler: { _ in
-//                    alert.dismiss(animated: true, completion: nil)
-//                })
-//                alert.addAction(act)
-//                self.present(alert, animated: true)
-//            case .failure(let error):
-//                print(error)
-//                let alert = UIAlertController(title: "ERROR", message: "\(String(describing: error))", preferredStyle: .alert)
-//
-//                let act = UIAlertAction(title: "Yes", style: .default, handler: { _ in
-//                    alert.dismiss(animated: true, completion: nil)
-//                })
-//                alert.addAction(act)
-//                self.present(alert, animated: true)
-//            }
-//        }
-        
-          networkingService.call(path: "/api_contents/get/wallpapers_categories", headers: nil, params: ["lang":preferredLanguage], httpMethod: .get) { (result :Result<(object: [Tag], unwrapped: Data), Error>) in
-        
-            switch result {
-            case .success(let tuple):
-                let obj = tuple.object
-                let data = String(data: tuple.unwrapped, encoding: .utf8)
-                let alert = UIAlertController(title: "SUCCESS", message: "OBJECT: \(obj)\nDATA: \(data ?? "")", preferredStyle: .alert)
-                
-                let act = UIAlertAction(title: "Yes", style: .default, handler: { _ in
-                    alert.dismiss(animated: true, completion: nil)
-                })
-                alert.addAction(act)
-                self.present(alert, animated: true)
-            case .failure(let error):
-                print(error)
-                var strError: String = ""
-                switch error as? NetworkingError {
-                case .unknown(let data)?:
-                    print(data)
-                    strError = String(data: data, encoding: .utf8)!
-                default:
-                    print(error)
-                    strError = String(describing: error)
-                    break
-                }
-                let alert = UIAlertController(title: "ERROR", message: "\(String(describing: strError))", preferredStyle: .alert)
-                
-                let act = UIAlertAction(title: "Yes", style: .default, handler: { _ in
-                    alert.dismiss(animated: true, completion: nil)
-                })
-                alert.addAction(act)
-                self.present(alert, animated: true)
-            }
-        }
-    }
-    
-    @IBAction func testRequest(_ sender: UIButton) {
-        dataRequest()
-    }
+	
+	let mocks: Bool = false
+	var networkingService: Networking {
+		return self.mocks ? NetworkingMockService() : NetworkingService(baseUrl: "https://jsonplaceholder.typicode.com")
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+	
+	func dataRequest() {
+		
+		
+		networkingService.call(path: "/posts", headers: nil, params: nil, httpMethod: .get) { (result :Result<(object: [Post]?, unwrapped: Data), Error>) in
+			
+			switch result {
+			case .success(let tuple):
+				let obj = tuple.object
+				let data = String(data: tuple.unwrapped, encoding: .utf8)
+				let alert = UIAlertController(title: "SUCCESS", message: "OBJECT: \(String(describing: obj))\nDATA: \(data ?? "")", preferredStyle: .alert)
+				
+				let act = UIAlertAction(title: "Yes", style: .default, handler: { _ in
+					alert.dismiss(animated: true, completion: nil)
+				})
+				alert.addAction(act)
+				self.present(alert, animated: true)
+			case .failure(let error):
+				print(error)
+				var strError: String = ""
+				switch error as? NetworkingError {
+				case .unknown(let data)?:
+					print(data)
+					strError = String(data: data, encoding: .utf8)!
+				default:
+					print(error)
+					strError = String(describing: error)
+					break
+				}
+				let alert = UIAlertController(title: "ERROR", message: "\(String(describing: strError))", preferredStyle: .alert)
+				
+				let act = UIAlertAction(title: "Yes", style: .default, handler: { _ in
+					alert.dismiss(animated: true, completion: nil)
+				})
+				alert.addAction(act)
+				self.present(alert, animated: true)
+			}
+		}
+	}
+	
+	@IBAction func testRequest(_ sender: UIButton) {
+		dataRequest()
+	}
 }
 
