d1cecc9fa9535323d49845835311e8331dd48bae
Hipolito Arias Cervero <hipolito.arias@kairosds.com>
2019-11-13 20:15:35 +0100

    refactor swiftlint

--- MonnoNetwork/NetworkingService.swift
+++ MonnoNetwork/NetworkingService.swift
@@ -38,7 +38,8 @@ public enum HTTPMethod {
     case get
     case post
     case patch
-    case multipart
+    case postMultipart
+    case patchMultipart
 }
 
 /// <#Description#>
@@ -299,17 +300,7 @@ public extension Networking {
         secureRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
 
         var data = Data()
-
-//        // Add the reqtype field and its value to the raw http request data
-//        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
-//        data.append("Content-Disposition: form-data; name=\"\(fieldName)\"\r\n\r\n".data(using: .utf8)!)
-//        data.append("\(fieldValue)".data(using: .utf8)!)
-//
-//        // Add the userhash field and its value to the raw http reqyest data
-//        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
-//        data.append("Content-Disposition: form-data; name=\"\(fieldName2)\"\r\n\r\n".data(using: .utf8)!)
-//        data.append("\(fieldValue2)".data(using: .utf8)!)
-
+        
         // Add the image data to the raw http request data
         data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
         data.append("Content-Disposition: form-data; name=\"photo\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
@@ -340,65 +331,14 @@ public extension Networking {
             request(method: "GET", path: path, headers: headers, urlParams: params, completion: completion)
         case .patch:
             request(method: "PATCH", path: path, headers: headers, bodyParams: params, completion: completion)
-        case .multipart:
+        case .postMultipart:
             request(image: image!, method: "POST", path: path, headers: headers, multipart: params, completion: completion)
+        
+        case .patchMultipart:
+            request(image: image!, method: "PATCH", path: path, headers: headers, multipart: params, completion: completion)
         }
     }
 }
-
-//extension URLRequest {
-//
-//    public mutating func setMultipartFormData(_ parameters: [String: String], encoding: String.Encoding, image: UIImage) throws {
-//
-//        let makeRandom = { UInt32.random(in: (.min)...(.max)) }
-//        let boundary = String(format: "------------------------%08X%08X", makeRandom(), makeRandom())
-//
-//        let filename = "photo"
-//
-//        let contentType: String = try {
-//            guard let charset = CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(encoding.rawValue)) else {
-//                throw MultipartFormDataEncodingError.characterSetName
-//            }
-//            return "multipart/form-data; charset=\(charset); boundary=\(boundary)"
-//            }()
-//        addValue(contentType, forHTTPHeaderField: "Content-Type")
-//
-//        httpBody = try {
-//            var body = Data()
-//
-//            for (rawName, rawValue) in parameters {
-//                if !body.isEmpty {
-//                    body.append("\r\n".data(using: .utf8)!)
-//                }
-//
-//                body.append("--\(boundary)\r\n".data(using: .utf8)!)
-//
-//                guard rawName.canBeConverted(to: encoding), let disposition = "Content-Disposition: form-data; name=\"\(rawName)\"\r\n".data(using: encoding) else {
-//                    throw MultipartFormDataEncodingError.name(rawName)
-//                }
-//                body.append(disposition)
-//
-//                body.append("\r\n".data(using: .utf8)!)
-//
-//                guard let value = rawValue.data(using: encoding) else {
-//                    throw MultipartFormDataEncodingError.value(rawValue, name: rawName)
-//                }
-//
-//                body.append(value)
-//            }
-//
-//            // Add the image data to the raw http request data
-//            body.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
-//            body.append("Content-Disposition: form-data; name=\"photo\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
-//            body.append("Content-Type: image/png\r\n\r\n".data(using: .utf8)!)
-//            body.append(image.pngData()!)
-//
-//            body.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
-//
-//            return body
-//            }()
-//    }
-//}
 
 public enum MultipartFormDataEncodingError: Error {
     case characterSetName
